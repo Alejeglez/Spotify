f33868dc701d64d4abfab89bd76e349220b20cf8
Alejandro <alejandrojeglezsantana@gmail.com>
2022-10-31 23:21:21 +0000

    Creación clase get artist, para obtener artistas

--- src/main/java/es/ulpgc/spotify/downloader/Main.java
+++ src/main/java/es/ulpgc/spotify/downloader/Main.java
@@ -1,6 +1,7 @@
 package es.ulpgc.spotify.downloader;
 
 import es.ulpgc.spotify.downloader.artist.Artist;
+import es.ulpgc.spotify.downloader.artist.GetArtist;
 import es.ulpgc.spotify.downloader.artistBase.ArtistBase;
 
 import java.util.ArrayList;
@@ -8,9 +9,12 @@ import java.util.Map;
 
 public class Main {
 
-    private static  ArrayList<ArtistBase> artists;
+    public static ArrayList<ArtistBase> artists;
 
     public static void main(String[] args) throws Exception {
+        createArtist();
+        GetArtist artist1 = new GetArtist(artists);
+        artist1.getArtists();
 
     }
 
@@ -23,10 +27,16 @@ public class Main {
         artists = new ArrayList<>();
 
         ArtistBase elvisPresley = new ArtistBase("Elvis Presley", "43ZHCT0cAZBISjO8DG9PnE", 82);
+        artists.add(elvisPresley);
         ArtistBase michaelJackson = new ArtistBase("Michael Jackson", "3fMbdgg4jU18AjLCKBhRSm", 85);
+        artists.add(michaelJackson);
         ArtistBase pharrellWilliams = new ArtistBase("Pharrell Williams", "2RdwBSPQiwcmiDo9kixcl8", 85);
+
         ArtistBase taylorSwift = new ArtistBase("Taylor Swift", "06HL4z0CvFAxyc27GXpf02", 100);
+        artists.add(taylorSwift);
         ArtistBase adele = new ArtistBase("Adele", "4dpBaseARuHxo51G3z768sgnrY", 88);
+        artists.add(adele);
+        artists.add(pharrellWilliams);
 
     }
 }
--- src/main/java/es/ulpgc/spotify/downloader/artist/GetArtist.java
+++ src/main/java/es/ulpgc/spotify/downloader/artist/GetArtist.java
@@ -1,6 +1,61 @@
 package es.ulpgc.spotify.downloader.artist;
 
+import es.ulpgc.spotify.downloader.SpotifyAccessor;
+import es.ulpgc.spotify.downloader.artistBase.ArtistBase;
+
+import java.util.List;
+import java.util.Map;
+
+
+import static spark.Spark.get;
+import static spark.Spark.halt;
+
+
 public class GetArtist {
 
+    public GetArtist(List<ArtistBase> artists) {
+        this.artists = artists;
+    }
 
+    private List<ArtistBase> artists;
+
+    public String getArtists() {
+        get("/spotify-api/v1", (request, response) -> {
+            response.header("content-type", "application/json");
+            String popularity = request.queryParams("popularity");
+            String query = "";
+
+
+
+            if (popularity == null) {
+                for(ArtistBase artista: artists) {
+                    query += artista.getId() + ",";
+                }
+
+                SpotifyAccessor accessor = new SpotifyAccessor();
+                String json = accessor.get("/artists?ids=" + query.substring(0, query.length()-1), Map.of());
+                return json;
+            }
+
+            try{
+                int popularityParams = Integer.parseInt(popularity);
+                for(ArtistBase artista: artists){
+                    if(artista.getPopularity() >= popularityParams){
+                        query += artista.getId() + ",";
+                    }
+                }
+                SpotifyAccessor accessor = new SpotifyAccessor();
+                String json = accessor.get("/artists?ids=" + query.substring(0, query.length()-1), Map.of());
+                return json;
+            }
+            catch (NumberFormatException e){
+                halt(400, "Bad type for popularity param");
+            }
+
+
+            return null;
+        });
+        return null;
+    }
 }
+
